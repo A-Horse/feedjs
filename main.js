f5f2dcaa2a9cecdd2c5337e165f85144951780d9
fwchen <crystal.tyan@gmail.com>
2017-04-25 14:52:20 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import program from 'commander';
+import loop from './lib/loop';
+
+program
+  .version('0.0.1')
+  .option('-p, --peppers', 'Add peppers')
+  .option('-P, --pineapple', 'Add pineapple')
+  .option('-b, --bbq-sauce', 'Add bbq sauce')
+  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .parse(process.argv);
+
+loop(["http://nullprogram.com/feed/", "https://www.raspberrypi.org/feed/"]);
