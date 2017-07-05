76c0ea3e9c92031c841b14c42f98707a1b3325eb
fwchen <crystal.tyan@gmail.com>
2017-07-05 22:57:13 +0800

    initial commit

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ export const startListenFeed = () => {
     [{url: "http://nullprogram.com/feed/"}, {url: "https://www.raspberrypi.org/feed/"}]
   ).subscribe(async (feed) => {
     const feedXml = await xml2js.parseString(feed.feedRawData);
+    console.log("feedXml = ", feedXml);
 
 
   });
