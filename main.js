cdca99e4cdca4862a6c650c5bbbe8b8e84b067d6
fwchen <crystal.tyan@gmail.com>
2017-07-10 21:34:27 +0800

    initial commit

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 import loop from './lib/loop';
-import md5 from 'md5'
+import md5 from 'md5';
 import xml2js from 'xml2js';
+import { parseFeed } from 'feed-parse-lib';
+
+// process.on('unhandledRejection', function(err) {
+//   console.error(err);
+// });
 
 export const startListenFeed = () => {
   // const hash
-  const subscription = loop(
-    [{url: "http://nullprogram.com/feed/"}, {url: "https://www.raspberrypi.org/feed/"}]
-  ).subscribe(async (feed) => {
-    const feedXml = await xml2js.parseString(feed.feedRawData);
-    console.log("feedXml = ", feedXml);
-
-
+  const hash = {};
+  const subscription = loop([
+    { url: 'http://nullprogram.com/feed/' },
+    { url: 'https://www.raspberrypi.org/feed/' }
+  ]).subscribe(async feed => {
+    console.log(feed.url);
+    // try {
+    //   const parsedFeed = await parseFeed(feed.feedRawData);
+    // } catch (error) {
+    //   console.error(error);
+    // }
   });
   return () => subscription.unsubscribe();
 };
