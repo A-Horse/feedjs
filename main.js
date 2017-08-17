83443f7183afd6964bdb19058607fcaf4ed11b5b
fwchen <crystal.tyan@gmail.com>
2017-08-17 10:48:34 +0800

    update

--- main.js
+++ main.js
@@ -2,26 +2,28 @@ import loop from './lib/loop';
 import md5 from 'md5';
 import xml2js from 'xml2js';
 import { parseFeed } from 'feed-parse-lib';
+import colors from 'colors';
 
 // process.on('unhandledRejection', function(err) {
 //   console.error(err);
 // });
 
-export const startListenFeed = () => {
-  // const hash
-  const hash = {};
-  const subscription = loop([
-    { url: 'http://nullprogram.com/feed/' },
-    { url: 'https://www.raspberrypi.org/feed/' }
-  ]).subscribe(async feed => {
-    console.log(feed.url);
-    // try {
-    //   const parsedFeed = await parseFeed(feed.feedRawData);
-    // } catch (error) {
-    //   console.error(error);
-    // }
+export const startFetchFeed = (feeds, listenFn) => {
+  const feedObservable = loop(feeds);
+  feedObservable.subscribe(async feed => {
+    try {
+      if (!feed) {
+        return;
+      }
+      const parsedFeed = await parseFeed(feed.feedRawData);
+      return listenFn(parsedFeed);
+    } catch (error) {
+      console.log(colors.bold.red('Something wrong when parseFeed'));
+      console.log(colors.bold.red(feed.url));
+      console.log(colors.bold.red(error));
+    }
   });
-  return () => subscription.unsubscribe();
+  return () => subscription.unsubscribe(); // TODO next complete
 };
 
-startListenFeed();
+export default startFetchFeed;
